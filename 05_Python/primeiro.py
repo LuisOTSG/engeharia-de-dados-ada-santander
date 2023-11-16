80a1a5275e46e622b459e8cec8f2f8f796f466d9
Luis Otávio <luis12osg@gmail.com>
2023-11-15 21:21:19 -0300

    feat: module 'Python' class 'Entrada e Saída de Dados'

---
+++ 05_Python/primeiro.py
@@ -0,0 +1,6 @@
+# Aula 2 - Entrada e Saída de Dados
+
+print('Hello, World!')
+print('Bem-vindo(a) ao curso de Python da Trilha de Engenharia de Dados')
+
+input('Qual a linguagem de programação você está estudando? ')
