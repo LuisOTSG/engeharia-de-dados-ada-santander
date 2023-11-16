363171012a1d7dfaa3e10a5746f9f5a8f354c0b2
Luis Otávio <luis12osg@gmail.com>
2023-11-15 21:22:16 -0300

    feat: module 'Python' class 'Operadores'

---
+++ 05_Python/operadores.py
@@ -0,0 +1,48 @@
+# Aula 4 - Operadores Aritméticos e Booleanos
+
+"""
+Operadores Matemáticas:
+    - Soma: +
+    - Subtração: -
+    - Multiplicação: *
+    - Divisão: /
+    - Resto: % (mod)
+    - Divisão Inteira: //
+    - Potenciação: **
+    - Radiciação: **(1/x) (onde x é o índice)
+"""
+
+num1 = 10
+num2 = 20
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(25 % 8)
+print(2 ** 3)
+print(8**(1/3))
+
+"""
+Operadores de Comparação:
+    - Maior que: >
+    - Maior ou igual a: >=
+    - Menor que: <
+    - Menor ou igual a: <=
+    - Igual: ==
+    - Diferente: != ou <>
+"""
+
+idade1 = 20
+idade2 = 25
+altura1 = 1.72
+altura2 = 1.60
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print('Python' == 'python')
+print('carro' != 'moto')
+print(altura1 >= altura2)
+print(altura2 > altura3)
